620802675a5cc933452688994f390366b8e271f3
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-02-13 14:39:32 -0300

    trabajop final

--- american.js
+++ american.js
@@ -19,64 +19,57 @@ class tamañoHamb {
 let carrito = [];
 let valorCompra = 0;
 //Array para agrupara las clases de hamburguesas
-const tiposDeHamburguesa = [
-    new TipoHamburguesa(1,"Argenta", [new tamañoHamb("simple",770), new tamañoHamb("doble",1000)],
-        "./img/burgers/argenta.png"),
-    new TipoHamburguesa(2,"Barbacoa", [new tamañoHamb("simple",770), new tamañoHamb("doble",1000)],
-        "./img/burgers/barbacoa.png"),
-    new TipoHamburguesa(3,"Big American", [new tamañoHamb("simple",690), new tamañoHamb("doble",920)],
-        "./img/burgers/bigamerican.png"),
-    new TipoHamburguesa(4,"Bluecheese", [new tamañoHamb("simple",700), new tamañoHamb("doble",930)],
-        "./img/burgers/bluecheese.png"),
-    new TipoHamburguesa(5,"Cheddar y Bacon", [new tamañoHamb("simple",770), new tamañoHamb("doble",1000)],
-        "./img/burgers/chedarybacon.png"),
-    new TipoHamburguesa(6,"Cheeseburger", [new tamañoHamb("simple",660), new tamañoHamb("doble",890)],
-        "./img/burgers/cheeseburger.png"),
-    new TipoHamburguesa(7,"Clasica", [new tamañoHamb("simple",690), new tamañoHamb("doble",920)],
-        "./img/burgers/clasica.png"),
-    new TipoHamburguesa(8,"Doble Cuarto", [new tamañoHamb("simple",690), new tamañoHamb("doble",920)],
-        "./img/burgers/doblecuarto.png"),
-    new TipoHamburguesa(9,"Extreme", [new tamañoHamb("simple",770), new tamañoHamb("doble",1000)],
-        "./img/burgers/extreme.png"),
-    new TipoHamburguesa(10,"Italy", [new tamañoHamb("simple",770), new tamañoHamb("doble",930)],
-        "./img/burgers/italy.png"),
-    new TipoHamburguesa(7,"Oklahoma", [new tamañoHamb("simple",770), new tamañoHamb("doble",930)],
-        "./img/burgers/oklahoma.png"),
-    new TipoHamburguesa(7,"Patty Melt", [new tamañoHamb("simple",980) ],
-        "./img/burgers/pattymelt.png"),
+let tiposDeHamburguesa = []; 
+
+const mostrarListado = async ()=>{
+    debugger
+    const response = await fetch("hambstock.json");
+    const data = await response.json()
     
-]; 
-
-let gridHamb = document.getElementById("grid-hamb")
-
-for(let TipoHamburguesa of tiposDeHamburguesa ){
-    let sizesPrices = "";
-    for(let tamañoHamb of TipoHamburguesa.sizes){
-        sizesPrices += ` <li class="list-group-item">${tamañoHamb.nombre} $${tamañoHamb.precio}<img onclick="selecHamburguesa(${TipoHamburguesa.num},'${tamañoHamb.nombre}')" src="img/carrito.png" class="carrito" alt="carrito"></li>`
+    for(let tipoHamburguesa of data){
+        let sizes = [];
+        for (let size of tipoHamburguesa.sizes){
+            sizes.push(new tamañoHamb(size.nombre, size.precio));
+        }
+        let hambnueva = new TipoHamburguesa (tipoHamburguesa.num, tipoHamburguesa.name, sizes, tipoHamburguesa.image)
+        tiposDeHamburguesa.push(hambnueva);
     }
 
-    let newcards = document.createElement("div")
-    newcards.innerHTML = `<div class="card" style="width: 10rem; height: 17rem;">
-    <img src=${TipoHamburguesa.image} class="card-img-top" alt="imagen hamburguesa">
-    <div>
-        <h5>${TipoHamburguesa.name}</h5>
-    </div>
-         <ul class="list-group list-group-flush">
-           ${sizesPrices}
-        </ul>
-    </div>`
-    gridHamb.appendChild(newcards)
+    let gridHamb = document.getElementById("grid-hamb")
 
-} 
+    for(let TipoHamburguesa of tiposDeHamburguesa ){
+        let sizesPrices = "";
+        for(let tamañoHamb of TipoHamburguesa.sizes){
+            sizesPrices += ` <li class="list-group-item">${tamañoHamb.nombre} $${tamañoHamb.precio}<img onclick="selecHamburguesa(${TipoHamburguesa.num},'${tamañoHamb.nombre}')" src="img/carrito.png" class="carrito" alt="carrito"></li>`
+        }
+
+        let newcards = document.createElement("div")
+        newcards.innerHTML = `<div class="card" style="width: 10rem; height: 17rem;">
+        <img src=${TipoHamburguesa.image} class="card-img-top" alt="imagen hamburguesa">
+        <div>
+            <h5>${TipoHamburguesa.name}</h5>
+        </div>
+            <ul class="list-group list-group-flush">
+            ${sizesPrices}
+            </ul>
+        </div>`
+        gridHamb.appendChild(newcards)
+
+    } 
+    
+}
+
+mostrarListado();
 
 
 
+//sección de funciones
 function selecHamburguesa (idTipoHamburguesa,nombreSize){
     let hambur = getHamb(idTipoHamburguesa);
     let sizeSelected = getSize(hambur,nombreSize)
     carrito.push([hambur,sizeSelected]);
     valorCompra += sizeSelected.precio;
-    document.getElementById("compraCliente").innerHTML = `Usted a seleccionado ${carrito.length} ${carrito.length > 1? "hamburguesas": "hamburguesa"} por un valor de $${valorCompra}`
+    document.getElementById("compraCliente").innerHTML = `Usted a seleccionado ${carrito.length}  ${carrito.length > 1? "hamburguesas": "hamburguesa"} por un valor de $${valorCompra}`
 
 }
 
@@ -91,138 +84,42 @@ function getSize(hambSelected, nombreSize){
 } 
 
 
-/*
 
+let finCompra = document.getElementById ("finCompra");
 
-//Array para ingresar las hamburgesesas seleccionados por el usuario
-let hambSelected = [];
- 
-
-//Funcion que ordena las hamb de mayor a menor precio
-function ordenPriceMayMen(array){
-    const mayorMenor = [].concat(array)
-    mayorMenor.sort((a,b) => b.price - a.price)
-   return mayorMenor;
-}
-
-
-//Funcion que ordena las hamb de menor a mayor precio
-function ordenPriceMenMay(array){
-    const menorMayor = [].concat(array)
-    menorMayor.sort((a,b) => a.price - b.price)
-    return menorMayor;
-}
-
-
-//Funcion que ordena las hamb por codigo
-function ordenNumHamb(array){
-    const codigoHamb = [].concat(array)
-    codigoHamb.sort((a,b) => a.num - b.num)
-    return codigoHamb;
-}
-
-
-//Funcion que ordena las hamb por abcdario
-function ordenAbcd(array){
-    const alfabetico = [].concat(array)
-    alfabetico.sort((a,b) => {
-          if(a.name > b.name) {
-            return 1
-          }
-          if (a.name < b.name) {
-            return -1
-          }
-          return 0;
-    })
-   return alfabetico;
-}
-
-
-//Declaramos esta funcion y la invocamos 2 veces, una para la edad y la otra para cant de hamb.
-function isCantInvalid(cant, min=0, max=1000){
-    return isNaN (cant) || cant < min || cant > max;
-}
-
-//Ingresamos el nombre del usuario.
-let nombre = prompt ("Ingrese su nombre");
-
-//Ingresamos la edad con la condicion de que si ponemos letras u otro valor me repita el ciclo.
-let edad = parseInt (prompt ("Ingrese su edad"));
-while (isCantInvalid(edad,18)) {
-   edad = parseInt (prompt ("Error en el valor ingresado, por favor ingrese su edad"));
-}
-
-//ingresamos cantidad de hamb con la condicion de que se repita el ciclo siempre y cuando
-//no pongamos el dato correcto: número o número entre 1 y 10, em ese caso activa segundo prompt.
-let cantidadHamb = parseInt ( prompt (`Bienvenido ${nombre} Cuantas hamburguesas quieres comprar? Tienes un maximo de 10`));
-while (isCantInvalid(cantidadHamb,1,10)) {
-    cantidadHamb = parseInt ( prompt ("Error en el valor ingresado. Ingrese un número, tienes un maximo de 10"));
-}
-
-//Menú que tiene el usuario para visualizar el orden de las hamburguesas.
-let visualizacion = parseInt (prompt (`Seleccione la manera en que usted quiere ver nuestras hamburguesas. \n1 - Codigo de Hamburguesa
-2 - Mayor precio\n3 - Menor Precio\n4 - Alfabeticamente`))
-while (isCantInvalid(visualizacion,1,4)) {
-    visualizacion = parseInt (prompt ("Error en el valor ingresado"));
-}
-
-
-let listadoHamb = "Nuestras hamburguesas ordenadas ";
-let hambOrdenas = [];
-switch (visualizacion) {
-    case 1:
-        listadoHamb += "por codigo son:\n";
-        hambOrdenas = ordenNumHamb(tiposDeHamburguesa)
-        break;
-    case 2:
-        listadoHamb += "por mayor precio son:\n";
-        hambOrdenas = ordenPriceMayMen(tiposDeHamburguesa)
-        break;
-    case 3:
-        listadoHamb += "por menor precio son:\n";
-        hambOrdenas = ordenPriceMenMay(tiposDeHamburguesa)
-        break;
-    case 4:
-        listadoHamb += "alfabeticamente son:\n";
-        hambOrdenas = ordenAbcd(tiposDeHamburguesa)
-        break;
-
-}
-
-//Foreach para recorrer el array y poder concatenar los elemntos y asi armar el listado
-hambOrdenas.forEach(element => {
-    listadoHamb += `- COD: ${element.num} - ${element.name} (${element.size}) - $ ${element.price}\n`;
-});
-listadoHamb += "Seleccione el codigo de la hamburguesa deseada"
-
-//Ciclo para seleccionar las hamburguesas
-while (cantidadHamb > hambSelected.length) {
-    let nunHambSelected = parseInt ( prompt (listadoHamb));  
-    let hamburguesa = hambOrdenas.find(hamb => hamb.num == nunHambSelected);
-    if (hamburguesa){
-        hambSelected.push(hamburguesa);
-    }else{
-        alert ("Codigo incorrecto")
-    }
-}
-
-// Aqui sumamos el precio de las hamburguesas seleccionadas.
-let precioTotal = 0;
-let hambCompradas = "Las hamburguesas seleccionas son: \n"
-hambSelected.forEach(element => {
-    precioTotal += element.price
-    hambCompradas += `${element.name} (${element.size})\n`
+finCompra.addEventListener("click", ()=>{
+    
 })
 
-//Mensaje final (alert) el precio total de las hamburguesas seleccionadas y un listado de lo que se compro.
-alert (`${hambCompradas} \n El precio total es $ ${precioTotal} \n GRACIAS POR SU COMPRA`);
+function finalizarCompra(){
+    let texto = `Compraste ${carrito.length} hamburguesas.
+    Por un valor total de $${valorCompra}`;
 
-
-
-*/
-
-
-
+    Swal.fire({
+        title: texto,
+        
+        
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: "No, volver a elegir",
+        confirmButtonText: 'Si, quiero comprar!'
+      }).then((result) => {
+        debugger;
+        if (result.isConfirmed) {
+            Swal.fire(
+                'A disfrutar',
+                'Muchas gracias por tu compra'
+              )
+        }else if(result.isDenied){
+            alert("Ejecuto un método para borrar toda la selección");
+            carrito=[];
+            valorCompra=0;
+            
+        }
+        
+      })
+}
 
 
 
--- modal.js
+++ modal.js
@@ -15,8 +15,25 @@ function registrarNombre (){
     localStorage.setItem("cliente",nombre.value);
     escribirNombre(nombre.value);
     myModal.hide();
+    Toastify({
+        text: "DEFINITIVAMENTE LAS HAMBURGUESAS MAS RICAS DEL MUNDO",
+    
+        duration: 3000,
+       
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "red",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
 }
 
 function escribirNombre(nombre){
     document.getElementById("saludoCliente").append(`Bienvenido ${nombre} a American Burger `);
 }
+
+
